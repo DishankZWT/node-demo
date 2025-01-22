361aa23a6da13c149a6adc576fda02816b819693
DishankZWT <dishank@zealousweb.com>
2025-01-22 14:55:47 +0530

    Update readEnv.js

--- exercises/day-1/readEnv.js
+++ exercises/day-1/readEnv.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-require('dotenv').config({path:'../../.env'})
+require('dotenv').config()
 
 const appName = process.env.APP_NAME;
 const appEnv = process.env.APP_ENV;
